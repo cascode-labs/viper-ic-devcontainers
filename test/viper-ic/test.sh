6b46e7df00eb5476d05f1c150647a6349c633843
Curtis Mayberry <Curtisma3@gmail.com>
2024-05-31 15:10:37 +0000

    Update tests

--- test/viper-ic/test.sh
+++ test/viper-ic/test.sh
@@ -6,9 +6,10 @@ source ~/.bashrc
 # Template specific tests
 check "distro" lsb_release -c
 check "xschem-version" xschem --version
-check "klayout-version" klayout -v
-check "conda-version" conda --version
-check "mamba-version" conda --version
+check "ngspice-version" ngspice -v
+# check "klayout-version" klayout -v
+# check "conda-version" conda --version
+# check "mamba-version" conda --version
 
 # Report result
 reportResults
